9138286e571ee6b65dbd4fb8ca73ab0239aa3688
Chiragparmar4 <chiragparmar7620@gmail.com>
2024-07-11 11:59:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+
+    window.addEventListener("scroll",function(){
+        header.classList.toggle("sticky",this.window.screenY > 130);
+    })
+
+    // Example using jQuery
+$('.menulist a').click(function() {
+    $('.menulist a').removeClass('active');
+    $(this).addClass('active');
+});
